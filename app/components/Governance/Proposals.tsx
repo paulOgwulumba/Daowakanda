ddeee0c29d5f337f8646ede53f03a8a241ddaba4
sylvamadu <54332482+sylvamadu@users.noreply.github.com>
2024-05-12 21:20:18 +0100

    final fixes on sorting proposals and filter from the latest

--- app/components/Governance/Proposals.tsx
+++ app/components/Governance/Proposals.tsx
@@ -6,33 +6,84 @@ import { useGovernanceActions } from '@/features/governance/actions/governance.a
 import { useRecoilValue } from 'recoil';
 import { ProposalsAtom } from '@/features/governance/state/governance.atom';
 import { CardVoteLoader } from './CardVoteLoader';
+import moment from 'moment';
 
 export const Proposals = () => {
   const [itemDeleted, setItemDeleted] = useState(false);
+  const [optionsActive, setOptionsActive] = useState<boolean>(false);
+  const [selected, setSelected] = useState('All');
   const [searchTerm, setSearchTerm] = useState('');
   const { getAllProposals } = useGovernanceActions();
   const proposals = useRecoilValue(ProposalsAtom);
 
   const filterProposals = () => {
     if (searchTerm) {
-      return proposals.filter((proposal) =>
-        proposal.name.toLowerCase().includes(searchTerm.toLowerCase()),
-      );
+      return [...proposals]
+        .sort((a, b) => b.id - a.id)
+        .filter((proposal) =>
+          proposal.name.toLowerCase().includes(searchTerm.toLowerCase()),
+        );
     }
 
-    return proposals;
+    return [...proposals].sort((a, b) => b.id - a.id);
   };
 
   useEffect(() => {
     getAllProposals();
   }, [itemDeleted]);
 
+  const voteEnded = (end_time: any) => {
+    const viewCurr = moment().format('YYYY-MM-DD HH:mm:ss');
+    const testEndDate = moment.utc(end_time).format('YYYY-MM-DD HH:mm:ss');
+    if (testEndDate <= viewCurr) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <div className={styles['body-section']}>
       <div className={styles['form']}>
-        <div className={styles['input']}>
-          <input type="text" placeholder="All" />
-          <IoIosArrowDown className={styles['icon']} />
+        <div className={styles['filter-container']}>
+          <div className={styles['selected']}>
+            {selected}
+            <IoIosArrowDown
+              className={styles['icon']}
+              onClick={() => setOptionsActive(!optionsActive)}
+            />
+          </div>
+          {optionsActive && (
+            <div className={styles['options']}>
+              <div
+                className={styles['option']}
+                onClick={() => {
+                  setSelected('All');
+                  setOptionsActive(false);
+                }}
+              >
+                All
+              </div>
+              <div
+                className={styles['option']}
+                onClick={() => {
+                  setSelected('Pending');
+                  setOptionsActive(false);
+                }}
+              >
+                Pending
+              </div>
+              <div
+                className={styles['option']}
+                onClick={() => {
+                  setSelected('Approved');
+                  setOptionsActive(false);
+                }}
+              >
+                Approved
+              </div>
+            </div>
+          )}
         </div>
         <div className={styles['input-search']}>
           <input
@@ -47,23 +98,61 @@ export const Proposals = () => {
       <div className={styles['main-section']}>
         {proposals.length <= 0 &&
           [1, 2, 3].map((item, index) => <CardVoteLoader key={index} />)}
-        {filterProposals().map((proposal, index) => (
-          <CardVote
-            key={index}
-            title={proposal?.name}
-            yesVote={proposal?.yes_count}
-            tag={proposal?.tag_id}
-            isActive={proposal?.is_active}
-            description={proposal?.description}
-            noVote={proposal?.no_count}
-            end_time={proposal?.end_time}
-            created_on={proposal?.created_on}
-            id={proposal?.id}
-            setItemDeleted={setItemDeleted}
-            wallet_address={proposal?.wallet_address}
-            proposalData={proposal}
-          />
-        ))}
+        {filterProposals().map((proposal, index) => {
+          if (selected == 'All' || selected == 'Pending') {
+            if (!voteEnded(proposal?.end_time)) {
+              return (
+                <CardVote
+                  key={index}
+                  title={proposal?.name}
+                  yesVote={proposal?.yes_count}
+                  tag={proposal?.tag_id}
+                  isActive={proposal?.is_active}
+                  description={proposal?.description}
+                  noVote={proposal?.no_count}
+                  end_time={proposal?.end_time}
+                  created_on={proposal?.created_on}
+                  id={proposal?.id}
+                  setItemDeleted={setItemDeleted}
+                  wallet_address={proposal?.wallet_address}
+                  proposalData={proposal}
+                />
+              );
+            }
+          }
+        })}
+      </div>
+
+      <div className={styles['main-section']}>
+        {proposals.length <= 0 &&
+          [1, 2, 3].map((item, index) => <CardVoteLoader key={index} />)}
+        {filterProposals().map((proposal, index) => {
+          if (
+            (selected == 'Approved' &&
+              proposal.yes_count > proposal.no_count) ||
+            selected == 'All'
+          ) {
+            if (voteEnded(proposal?.end_time)) {
+              return (
+                <CardVote
+                  key={index}
+                  title={proposal?.name}
+                  yesVote={proposal?.yes_count}
+                  tag={proposal?.tag_id}
+                  isActive={proposal?.is_active}
+                  description={proposal?.description}
+                  noVote={proposal?.no_count}
+                  end_time={proposal?.end_time}
+                  created_on={proposal?.created_on}
+                  id={proposal?.id}
+                  setItemDeleted={setItemDeleted}
+                  wallet_address={proposal?.wallet_address}
+                  proposalData={proposal}
+                />
+              );
+            }
+          }
+        })}
       </div>
     </div>
   );
