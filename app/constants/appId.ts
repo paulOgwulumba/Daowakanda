2264d47607dc9fe901c35b4d462880eb3629c6ad
Adeyemi Sadiq <adexsadiq01@gmail.com>
2024-07-12 08:56:40 +0100

    appID change

--- app/constants/appId.ts
+++ app/constants/appId.ts
@@ -1,5 +1,5 @@
-export const APP_ID = 681119865;
+export const APP_ID = 693559212; //681119865;
 export const ASSET_ID = 627600054; //591099355
 
-//mainnet = 2033059214; 
+//mainnet = 2033059214;
 //testnet = 681119865;
